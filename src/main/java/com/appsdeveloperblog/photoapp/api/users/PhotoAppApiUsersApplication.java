7fd54fe151116d05ff8514b83f779b93cfecf060
Arnold Martinez <arnoldmartinez@criptext.com>
2022-05-04 17:48:13 -0500

    Enable Eureka Client Cap. 43

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -2,7 +2,9 @@ package com.appsdeveloperblog.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PhotoAppApiUsersApplication {
 
