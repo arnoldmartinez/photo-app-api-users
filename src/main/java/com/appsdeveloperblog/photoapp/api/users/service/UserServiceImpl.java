f4282522a6f36475ab84f8c124e8033adbb06b07
Arnold Martinez <arnoldmartinez@criptext.com>
2022-05-12 07:16:33 -0500

    Generate unique public user id
    Cap. 80

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UserServiceImpl.java
@@ -2,10 +2,14 @@ package com.appsdeveloperblog.photoapp.api.users.service;
 
 import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
 
+import java.util.UUID;
+
 public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto CreateUser(UserDto userDetails) {
+        userDetails.setUserId(UUID.randomUUID().toString());
+
         return null;
     }
 }
