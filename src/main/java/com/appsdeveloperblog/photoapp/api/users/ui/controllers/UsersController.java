7dd678870b4050b4178b4b04032ca1ca0c514fa1
Arnold Martinez <arnoldmartinez@criptext.com>
2022-05-12 06:40:53 -0500

    Implementing the create user request model class
    Cap. 75

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,11 +1,9 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
+import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -20,7 +18,7 @@ public class UsersController {
     }
 
     @PostMapping
-    public String createUser() {
+    public String createUser(@RequestBody CreateUserRequestModel userDetails) {
         return "Create use method is called";
     }
 }
