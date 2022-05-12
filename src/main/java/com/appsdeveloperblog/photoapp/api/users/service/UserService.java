f42e74cebc91e05c21ddb2ea90e7eae479012b7f
Arnold Martinez <arnoldmartinez@criptext.com>
2022-05-12 07:06:17 -0500

    Create a shared DTO class
    Cap. 79

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UserService.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UserService.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.photoapp.api.users.service;
 
+import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
+
 public interface UserService {
     UserDto CreateUser(UserDto userDetails);
 }
