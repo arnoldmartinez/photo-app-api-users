360b9e1ca606a20b12356e45226a9245183cead3
Arnold Martinez <arnoldmartinez@criptext.com>
2022-05-18 22:33:09 -0500

    AuthenticationFilter. Implementing successfulAuthentication().
    Cap. 96

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/security/AuthenticationFilter.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/security/AuthenticationFilter.java
@@ -1,10 +1,18 @@
 package com.appsdeveloperblog.photoapp.api.users.security;
 
+import com.appsdeveloperblog.photoapp.api.users.service.UserService;
+import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
 import com.appsdeveloperblog.photoapp.api.users.ui.model.LoginRequestModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -13,8 +21,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
+
+
+
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private UserService userService;
+
+    private Environment environment;
+
+    @Autowired
+    public AuthenticationFilter(UserService userService, Environment environment, AuthenticationManager authenticationManager) {
+        this.userService = userService;
+        this.environment = environment;
+        super.setAuthenticationManager(authenticationManager);
+    }
 
     public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) throws AuthenticationException {
         try {
@@ -32,5 +55,18 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         }
     }
 
-    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain, Authentication auth) throws IOException, ServletException {}
+    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain, Authentication auth) throws IOException, ServletException {
+        String userName = ((User) auth.getPrincipal()).getUsername();
+
+        UserDto userDetails = userService.getUserDetailsByEmail(userName);
+
+        String token = Jwts.builder()
+        		.setSubject(userDetails.getUserId())
+        		.setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(environment.getProperty("token.expiration_time"))))
+                .signWith(SignatureAlgorithm.HS512, environment.getProperty("token.secret"))
+        		.compact();
+
+        res.addHeader("token", token);
+        res.addHeader("userId", userDetails.getUserId());
+    }
 }
