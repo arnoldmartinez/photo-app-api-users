b0ee2f662427787d2fde4b968952068f3b71611d
Arnold Martinez <arnoldmartinez@criptext.com>
2022-07-05 22:52:05 -0500

    Make users microservice call albums microservice
    Section 24: Microservices Communication Cap. 170

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -3,11 +3,13 @@ package com.appsdeveloperblog.photoapp.api.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhotoAppApiUsersApplication {
 
     public static void main(String[] args) {
@@ -18,4 +20,11 @@ public class PhotoAppApiUsersApplication {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
+
--- src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UserServiceImpl.java
@@ -3,16 +3,23 @@ package com.appsdeveloperblog.photoapp.api.users.service;
 import com.appsdeveloperblog.photoapp.api.users.data.UserEntity;
 import com.appsdeveloperblog.photoapp.api.users.data.UsersRepository;
 import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
+import com.appsdeveloperblog.photoapp.api.users.ui.model.AlbumResponseModel;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -22,10 +29,16 @@ public class UserServiceImpl implements UserService {
 
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    RestTemplate restTemplate;
+
+    Environment environment;
+
     @Autowired
-    public UserServiceImpl(UsersRepository usersRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserServiceImpl(UsersRepository usersRepository, BCryptPasswordEncoder bCryptPasswordEncoder, RestTemplate restTemplate, Environment environment) {
         this.usersRepository = usersRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.restTemplate = restTemplate;
+        this.environment = environment;
     }
 
     @Override
@@ -67,9 +80,18 @@ public class UserServiceImpl implements UserService {
     public UserDto getUserByUserId(String userId) {
         UserEntity userEntity = usersRepository.findByUserId(userId);
 
-        if (userEntity == null) throw new UserServiceException("User not found");
+        if (userEntity == null) throw new UsernameNotFoundException("User not found");
 
         UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
+
+        String albumsUrl = String.format(environment.getProperty("albums.url"), userId);
+
+        ResponseEntity<List<AlbumResponseModel>> albumsListResponse = restTemplate.exchange(albumsUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<AlbumResponseModel>>() {
+        });
+
+        List<AlbumResponseModel> albumsList = albumsListResponse.getBody();
+
+        userDto.setAlbums(albumsList);
 
         return userDto;
     }
