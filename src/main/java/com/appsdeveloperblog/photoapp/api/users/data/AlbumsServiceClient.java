9d3f5833341f088b633b30ee4a0c4234d01ebbfc
Arnold Martinez <arnoldmartinez@criptext.com>
2022-07-07 21:50:44 -0500

    @Retry annotation
    Section 27: Microservices communication. Resilience4j - Retry
    Cap. 192

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumsServiceClient.java
@@ -2,8 +2,8 @@ package com.appsdeveloperblog.photoapp.api.users.data;
 
 import com.appsdeveloperblog.photoapp.api.users.ui.model.AlbumResponseModel;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -14,6 +14,7 @@ import java.util.List;
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{id}/albums")
+    @Retry(name = "album-ws")
     @CircuitBreaker(name="albums-ws", fallbackMethod="getAlbumsFallback")
     public List<AlbumResponseModel> getAlbums(@PathVariable String id);
 
