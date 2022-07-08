c77170a3584739f75ae6062c800a3948a422882b
Arnold Martinez <arnoldmartinez@criptext.com>
2022-07-07 20:10:28 -0500

    Removing Hystrix circuit breaker
    Section 26: Microservices communication. Resilience4j - Circuit Breaker
    Cap. 185

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -3,7 +3,6 @@ package com.appsdeveloperblog.photoapp.api.users;
 import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,7 +13,6 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableCircuitBreaker
 public class PhotoAppApiUsersApplication {
 
     public static void main(String[] args) {
