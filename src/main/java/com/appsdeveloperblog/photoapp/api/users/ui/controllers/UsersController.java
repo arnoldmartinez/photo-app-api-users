7b01af12d9cba46126f745f431823358b2ca3a05
Arnold Martinez <arnoldmartinez@criptext.com>
2022-07-18 21:16:46 -0500

    @PreAuthorize annotation example
    Section 33: Downstream Microservice and Method-Level Security Cap. 256

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -12,6 +12,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -48,7 +49,9 @@ public class UsersController {
         return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
     }
 
+
     @GetMapping(value="/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    @PreAuthorize("principal == #userId")
     public ResponseEntity<UserResponseModel> getUser(@PathVariable("userId") String userId) {
         UserDto userDto = userService.getUserByUserId(userId);
 
