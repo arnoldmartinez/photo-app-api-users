c3b97a8d8aa0326f6c9c7997c3ec010f8f5e0da2
Arnold Martinez <arnoldmartinez@criptext.com>
2022-07-18 21:40:52 -0500

    @PostAuthorize annotation example
    Section 33: Downstream Microservice and Method-Level Security Cap. 258

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -12,7 +12,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -51,7 +51,8 @@ public class UsersController {
 
 
     @GetMapping(value="/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-    @PreAuthorize("principal == #userId")
+    //@PreAuthorize("principal == #userId")
+    @PostAuthorize("principal == returnObject.body.userId")
     public ResponseEntity<UserResponseModel> getUser(@PathVariable("userId") String userId) {
         UserDto userDto = userService.getUserByUserId(userId);
 
