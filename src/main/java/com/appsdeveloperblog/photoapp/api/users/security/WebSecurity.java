1afd150cd246090515d4b30da8cf9ca41bdf4223
Arnold Martinez <arnoldmartinez@criptext.com>
2022-07-18 20:13:52 -0500

    Enable method-level security
    Section 33: Downstream microservice and method-level security Cap. 255

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -2,16 +2,16 @@ package com.appsdeveloperblog.photoapp.api.users.security;
 
 import com.appsdeveloperblog.photoapp.api.users.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
